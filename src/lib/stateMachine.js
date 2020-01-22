669dd6f001ed8977861a73471b4108118612a8fb
Stephen Reed <domotang@hotmail.com>
2020-01-22 16:35:36 -0600

    added a state machine library to control modes and animations

---
+++ src/lib/stateMachine.js
@@ -0,0 +1,19 @@
+export default function StateMachine(stateMachineDef) {
+  return {
+    value: stateMachineDef.initValue,
+    transition(event, opts) {
+      if (event === this.value) return this.value;
+      var curtStateDef = stateMachineDef[this.value];
+      var destStateTrans = curtStateDef.transitions[event];
+      if (!destStateTrans) return this.value;
+      this.value = destStateTrans.target;
+      destStateTrans.action(opts);
+      curtStateDef.actions.onExit();
+      stateMachineDef[this.value].actions.onEnter();
+      return this.value;
+    },
+    is() {
+      return this.value;
+    }
+  };
+}
